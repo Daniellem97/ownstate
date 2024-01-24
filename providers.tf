3a64b8d28542b9a047f5501c23755862ab185c43
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-24 22:16:55 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstatebucketsl"
-    key            = "new_application.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-west-2"
   }
 }
